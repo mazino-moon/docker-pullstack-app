54afdb3d7a43f4e8618a7cdeb2afec6f559d066f
mazino-moon <mazinomoon@gmail.com>
2022-02-25 02:12:43 +0900

    aws first

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,20 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "mazino123",
-  database: "myapp",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 exports.pool = pool;
+
+//개발용
+// var pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "mysql",
+//   user: "root",
+//   password: "mazino123",
+//   database: "myapp",
+// });
+// exports.pool = pool;
