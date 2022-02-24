08e502cd453dd7943fe8cf5b8846bf258c187b57
mazino-moon <mazinomoon@gmail.com>
2022-02-25 04:41:23 +0900

    fix js

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,18 @@ const app = express();
 app.use(bodyParser.json());
 
 //테이블생성
-db.pool.query(`CREATE TABLE lists(
-id INTEGER_AUTO_INCREMENT,
-value TEXT,
-PRIMARY KEY (id)
-)`),
-  (err, results, fileds) => {
-    console.log("results", results);
-  };
-
-  db.pool.query(`INSERT INTO VALUES ("xxxx")
-    `),
-      (err, results, fileds) => {
-        console.log("results", results);
-      };
+db.pool.query(`CREATE TABLE lists (
+  id INTEGER AUTO_INCREMENT,
+  value TEXT, 
+  PRIMARY KEY (id)
+)`, (err, results, fileds) => {
+  console.log('results', results)
+})
+//DB lists 테이블에 있는 모든 데이터를 프론트 서베에 보내주기 
+app.get('/api/hi', function (req, res) {
+  //데이테베이스에서 모든 정보 가져오기 
+ res.status(200).send('good')
+})
 
 app.get("/api/values", function (req, res) {
   db.pool.query("SELECT * FROM lists;", (err, results, fileds) => {
